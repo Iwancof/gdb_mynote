659a16cb27d47da32df32d067a2339cd14118aae
iwancof <rock.iwancof@gmail.com>
2021-12-28 20:39:44 +0900

    with name

--- addr_note.py
+++ addr_note.py
@@ -5,11 +5,12 @@ import json
 file_name = "address_note.json"
 notes = {}
 
+
 class note_command(gdb.Command):
     def __init__(self):
         super(note_command, self).__init__("note", gdb.COMMAND_RUNNING)
 
-        if (os.path.exists(file_name)):
+        if os.path.exists(file_name):
             print("json note file found.")
             with open(file_name, "r") as f:
                 notes = json.loads(f.read())
@@ -17,18 +18,31 @@ class note_command(gdb.Command):
             notes = {}
 
     def invoke(self, arg, from_tty):
-        if(arg == ''):
+        if arg == "":
             print("No arguments were given")
             return
 
-        argv = arg.split(' ')
-        
-        if (len(argv) == 1):
+        argv = arg.split(" ")
+
+        if len(argv) == 1:
             name = argv[0]
             print("save stack pointer as {}".format(name))
             rsp = gdb.newest_frame().read_register("rsp")
 
             notes[name] = int("{}".format(rsp), 16)
+        elif len(argv) == 2:
+            name = argv[0]
+            value_str = argv[1]
+
+            if value_str[0:2] == "0x":
+                value = int(value_str, 16)
+                print("save {} as {}".format(hex(value), name))
+            else:
+                value = int(value_str)
+                print("save {} as {}".format(value, name))
+
+            notes[name] = value
+
 
 class save_note_command(gdb.Command):
     def __init__(self):
@@ -38,6 +52,7 @@ class save_note_command(gdb.Command):
         with open(file_name, "w") as f:
             f.write(json.dumps(notes))
         print("dump success")
+
 
 class show_note_command(gdb.Command):
     def __init__(self):
